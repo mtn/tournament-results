e160ce936b3f53c7fe47bb8aef75588c88b96c07
Michael Noronha <michaeltnoronha@gmail.com>
2017-01-08 15:42:07 -0600

    Updates to tournament.sql and readme

--- tournament.sql
+++ tournament.sql
@@ -1,14 +1,8 @@
 -- Table definitions for the tournament project.
---
--- Put your SQL 'create table' statements in this file; also 'create view'
--- statements if you choose to use it.
---
--- You can write comments in this file by starting them with two dashes, like
--- these lines here.
 
 
-/* DROP DATABASE IF EXISTS tournament; */
-/* CREATE DATABASE tournament; */
+DROP DATABASE IF EXISTS tournament;
+CREATE DATABASE tournament;
 \c tournament
 
 CREATE TABLE players (
@@ -16,6 +10,5 @@ CREATE TABLE players (
     name text );
 
 CREATE TABLE matches (
-    match_id serial PRIMARY KEY,
-    winner serial,
-    loser serial );
+    winner serial REFERENCES players,
+    loser serial REFERENCES players);
